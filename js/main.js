ff179677a5eb2665f5600a774c3e0d9389f31a59
Christopher Price <pricechrispy@users.noreply.github.com>
2019-03-10 18:51:55 -0700

    jquery fix; ui tweaks; setup button handlers

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@
 let _node_version = process.versions.node;
 let _chrome_version = process.versions.chrome;
 let _electron_version = process.versions.electron;
+let _jquery_version = $().jquery;
+
+console.log( 'Node v' + _node_version );
+console.log( 'Chrome v' + _chrome_version );
+console.log( 'Electron v' + _electron_version );
+console.log( 'jQuery v' + _jquery_version );
+
+let handle_open_settings = function() {
+    
+};
+
+let handle_get_password = function() {
+    $('span', this).html('Please<br>Wait...');
+};
+
+$('.action-settings').on( 'click', handle_open_settings );
+$('.main .take-action').on( 'click', handle_get_password );
---
+++ js/prep_node_integration.js
@@ -0,0 +1,7 @@
+
+// https://electronjs.org/docs/faq#i-can-not-use-jqueryrequirejsmeteorangularjs-in-electron
+
+window.nodeRequire = require;
+delete window.require;
+delete window.exports;
+delete window.module;
